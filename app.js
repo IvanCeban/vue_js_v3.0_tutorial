abdb784af6d874efcaebc04d02ff13a48bdc72d8
IC <himac!>
2021-05-10 11:27:30 +0300

    update

--- app.js
+++ app.js
@@ -9,8 +9,31 @@ const App = { // объект содержит настройки будущег
   },
 
   methods: {
-    inputChangeHandler (event) {
-      this.inputValue = event.target.value
+    addNewNote () {
+      if(this.inputValue) {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    removeNote (idx) {
+      this.notes.splice(idx, 1)
+    },
+    toUpperCase(item) {
+      return item.toUpperCase()
+    }
+  },
+
+  computed: {
+    doubleCount() {
+      return this.notes.length * 2
+    }
+  },
+
+  watch: {
+    inputValue (value) {
+      if(value.length > 10) {
+        this.inputValue = ''
+      }
     }
   }
 }
